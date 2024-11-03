e0e568db7c92155e0febd16696718020a6bf9028
Nagy Máté <morsakk.mate@gmail.com>
2024-11-03 13:28:12 +0100

    11.1

--- 3x3/script.js
+++ 3x3/script.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function(){
 
     let button = document.getElementById("button");
     let container = document.getElementById("container");
@@ -9,17 +9,13 @@ window.onload = function() {
     button.onclick = function random(){
         container.innerHTML = "";
         const usedNumbers = [];
-        console.log(usedNumbers);
-        for(let i = 1; i < 10; i++)
+        for(let i = 0; i < 9; i++)
         {
-            let rnd = Math.floor(Math.random() * 9);
-            console.log("első random: ", rnd);
+            let rnd = Math.floor(Math.random() * 9 + 1);
             while(usedNumbers.includes(rnd)){
-                rnd = Math.floor(Math.random() * 9);
-                console.log("while random: ", rnd);
+                rnd = Math.floor(Math.random() * 9 + 1);
             }
             usedNumbers.push(rnd);
-            console.log(usedNumbers);
             container.innerHTML += `<div class="grid-item">${rnd} </div>`;
         }
     }
